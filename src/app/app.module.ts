ef845084f447bfc19597dc124fd68fdff94bb691
ricky <riky.andriamaharo@aqm-online.com>
2018-02-15 22:10:14 +0100

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {  FormsModule, ReactiveFormsModule } from '@angular/forms'
+
+
+import { AppComponent } from './app.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { CourseFormComponent } from './course-form/course-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ContactFormComponent,
+    CourseFormComponent,
+    SignupFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
